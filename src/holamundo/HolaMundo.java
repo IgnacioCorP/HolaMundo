3cc22597dd26d48de76133efcfcd9b6758a03395
Ignacio Correa <nachitoficopal@gmail.com>
2022-09-28 12:35:20 +0200

    HolaMundo
    Inicio de curso 2Dam

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,15 @@
+
+
+package holamundo;
+
+import static java.time.Clock.system;
+
+    
+public class HolaMundo {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+
+}
